43781adc56eb1d2f0a65a4adb4a2aa8935af84b6
Justin Locsei <justin.locsei@oberlin.edu>
2012-02-09 11:51:43 -0500

    Reverted Loader Name
    To make the language-file generation work properly, the loader needs to match the name of its containing folder.

--- mu-plugins/class-blogs.php
+++ mu-plugins/class-blogs.php
@@ -26,6 +26,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-require_once( dirname(__FILE__) . '/class-blogs/loader.php' );
+require_once( dirname(__FILE__) . '/class-blogs/class-blogs.php' );
 
 ?>
